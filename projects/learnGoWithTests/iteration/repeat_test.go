2015d2c45e2b615447e9fbc194c2da035c6b6063
balleon <67829177+balleon@users.noreply.github.com>
2025-07-04 11:20:51 +0200

    feat: learnGoWithTests iteration (#15)

---
+++ projects/learnGoWithTests/iteration/repeat_test.go
@@ -0,0 +1,26 @@
+package iteration
+
+import "testing"
+import "fmt"
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a", 5)
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for b.Loop() {
+		Repeat("a", 5)
+	}
+}
+
+
+func ExampleRepeat() {
+	repeated := Repeat("a", 5)
+	fmt.Println(repeated)
+	// Output: aaaaa
+}
